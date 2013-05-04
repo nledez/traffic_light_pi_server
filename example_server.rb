bf1123dc06e4bca993d58f1164279bfb05bdcd25
Nicolas Ledez <github@ledez.net>
2013-05-04 11:53:30 +0200

    Put all line for my pi

--- example_server.rb
+++ example_server.rb
@@ -4,14 +4,23 @@ require File.join(File.dirname(__FILE__), 'lib', 'traffic_light_pi_server.rb')
 class TrafficLightPiServer < Sinatra::Base
   configure do
     @@line_map = {
-      0 => {
-        :red => 4,
-        :green => 5,
-      },
-      1 => {
+      :devant => {
         :red => 12,
         :orange => 13,
         :green => 14,
+      },
+      :gauche => {
+        :red => 4,
+        :green => 5,
+      },
+      :fond => {
+        :red => 6,
+        :orange => 10,
+        :green => 11,
+      },
+      :droite => {
+        :red => 0,
+        :green => 3,
       },
     }
     init_lights
