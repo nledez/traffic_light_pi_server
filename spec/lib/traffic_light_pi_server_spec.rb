b38944f46d0b9aa1c4b9b889a99d277f8a0a9ea1
Nicolas Ledez <github@ledez.net>
2013-05-08 22:12:21 +0200

    Spec refactoring

--- spec/lib/traffic_light_pi_server_spec.rb
+++ spec/lib/traffic_light_pi_server_spec.rb
@@ -31,27 +31,10 @@ describe "My Traffic light server" do
     last_response.should be_ok
   end
 
-  it "should get 0 for /front/red" do
+  it "should change status for different line & light" do
+    get '/front/green/0'
     get '/front/red/0'
-    get '/front/red'
-    last_response.should be_ok
-    last_response.body.should == "14:0"
-  end
-
-  it "should get 0 for /left/red" do
     get '/left/red/0'
-    get '/left/red'
-    last_response.body.should == "5:0"
-  end
-
-  it "should get 0 for /front/green" do
-    get '/front/green/0'
-    get '/front/green'
-    last_response.body.should == "12:0"
-  end
-
-  it "should change color for /front/green" do
-    get '/front/green/0'
 
     get '/front/green'
     last_response.body.should == "12:0"
@@ -67,11 +50,6 @@ describe "My Traffic light server" do
 
     get '/front/green'
     last_response.body.should == "12:0"
-  end
-
-  it "should change color for /front/red and keep green status" do
-    get '/front/green/0'
-    get '/front/red/0'
 
     get '/front/red'
     last_response.body.should == "14:0"
@@ -84,20 +62,6 @@ describe "My Traffic light server" do
 
     get '/front/green'
     last_response.body.should == "12:0"
-  end
-
-  it "should change color for /left/red and keep /front/red status" do
-    get '/front/red/0'
-    get '/left/red/0'
-
-    get '/front/red'
-    last_response.body.should == "14:0"
-
-    get '/front/red/1'
-    last_response.body.should == "14:1"
-
-    get '/front/red'
-    last_response.body.should == "14:1"
 
     get '/left/red'
     last_response.body.should == "5:0"
@@ -114,13 +78,11 @@ describe "My Traffic light server" do
     last_response.status.should == 404
   end
 
-  it "should return a custom 404 error page" do
+  it "should return a custom 404 & 500 error pages" do
     get '/left/red/bad/url'
     last_response.status.should == 404
     last_response.body.should =~ /traffic-lights-lost/
-  end
 
-  it "should return a custom 500 error page" do
     get '/left/red/4'
     last_response.status.should == 500
     last_response.body.should =~ /traffic-lights-ko/
